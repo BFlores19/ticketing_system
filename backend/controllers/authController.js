501c0625de76f7b56e08a1102aaeb0e5dda79946
davidrivera593 <131416582+davidrivera593@users.noreply.github.com>
2026-01-22 16:20:53 -0700

    Email feature when account is created. Need to make a OTP feature still so they are forced to reset it

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const sendEmail = require("../services/emailService");
+
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -54,6 +56,35 @@ exports.register = async (req, res) => {
     //   role,
     //   // asu_id,
     // });
+
+    if (created) {
+      try {
+        const subject = "Welcome to the ASU Capstone Help Desk System";
+        const emailBody = 
+        `
+        Hello ${name},
+
+        Your account has been created for the ASU Capstone Help Desk System.
+
+        Login at: https://helpdesk.asucapstonetools.com/login 
+
+        Email: ${email}
+        Password: ${password}
+        Role: ${role}
+
+        Please change your password after your first login. Password is generated for one time use.
+
+        If you have any question or need assistance, please contact your instructor or reach out for assistance
+
+        Best regards,
+        ASU Capstone Help Desk Team`;
+
+        await sendEmail(email, subject, emailBody);
+        console.log("Welcome email sent to", email);
+      } catch (emailError) {
+        console.error(`Failed to send email to ${email}:`, emailError);
+      }
+    }
 
     if (created) return res.status(201).json({ created: true, user });
     return res.status(200).json({ created: false, user });
